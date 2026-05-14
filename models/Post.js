14a8c0164220dc51d0e37c4996bec6eab46ac908
vajdavitya001-stack <vajdavitya001@gmail.com>
2026-05-14 12:42:31 +0200

    Kódhibák javítása

--- models/Post.js
+++ models/Post.js
@@ -1,22 +1,37 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require ('../db');
+
+const sequelize = require('../db');
+
 const Post = sequelize.define('Post', {
+
     title: {
+
         type: DataTypes.STRING,
+
         allowNull: false
-    }, 
+
+    },
 
     author: {
+
         type: DataTypes.STRING,
+
         allowNull: false
-    }, 
+
+    },
 
     content: {
+
         type: DataTypes.TEXT,
+
         allowNull: false
+
     }
-},{
-    timestamps:false
-    
+
+}, {
+
+    timestamps: false
+
 });
+
 module.exports = Post;
