68f6886345546dae193f8b746f3468d2c8bd543c
vajdavitya001-stack <vajdavitya001@gmail.com>
2026-05-14 12:42:11 +0200

    Kódhibák javítása

--- models/Comment.js
+++ models/Comment.js
@@ -1,14 +1,29 @@
-const { DataTypes } = require ('sequelize');
-const sequelize = require ('../db');
+const { DataTypes } = require('sequelize');
+
+const sequelize = require('../db');
+
 const Comment = sequelize.define('Comment', {
+
     username: {
+
         type: DataTypes.STRING,
+
         allowNull: false
+
     },
 
     text: {
+
         type: DataTypes.TEXT,
+
         allowNull: false
+
     }
+
+}, {
+
+    timestamps: false
+
 });
+
 module.exports = Comment;
